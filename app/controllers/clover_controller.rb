02f66328a6278dcc2aaae1cf16f9490d6082389c
Narbe Voskanian <narbevoskanian@gmail.com>
2015-05-27 00:15:43 -0700

    more testing

--- app/controllers/clover_controller.rb
+++ app/controllers/clover_controller.rb
@@ -1,6 +1,6 @@
 class CloverController < ApplicationController
   def callback
-    render json: current_user.parse_clover_code(params[:code])
+    render text: current_user.parse_clover_code(params[:code])[:access_token]
   end
 
   def authorize
