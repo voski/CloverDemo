f65c3d1913ee0c606583e0119d38ee2a5fba990e
Narbe Voskanian <narbevoskanian@gmail.com>
2015-05-27 10:21:59 -0700

    more

--- app/controllers/clover_controller.rb
+++ app/controllers/clover_controller.rb
@@ -1,6 +1,6 @@
 class CloverController < ApplicationController
   def callback
-    render json: current_user.parse_clover_code(params[:code], params[:merchant_id]).response
+    render json: current_user.parse_clover_code(params[:code], params[:merchant_id]).response.body
   end
 
   def authorize
